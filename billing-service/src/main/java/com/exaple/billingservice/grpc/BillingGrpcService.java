4f8c48e9c21b9e0d58c4498704607483024817dc
Smim7 <somayamorshed7@gmail.com>
2025-08-06 13:44:48 -0400

    created grpc and proto

---
+++ billing-service/src/main/java/com/exaple/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,32 @@
+package com.exaple.billingservice.grpc;
+
+import billing.BillingResponse;
+import billing.BillingServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
+
+    @Override
+    public void CreateBillingAccount(billing.BillingRequest billingRequest,
+                                     StreamObserver<BillingResponse> responseObserver){
+
+    log.info("CreateBillingAccount request receive: {}", billingRequest.toString());
+
+    //business logic-2.g save to database,perform calculates etc
+
+        BillingResponse response=BillingResponse.newBuilder()
+                .setAccountId("1234")
+                .setStatus("ACTIVE")
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+
+    }
+
+}
